d8ca7e0aa5d2e593c52e08135e85573e746bd3c8
SOVANORAK <norakeusever@gmail.com>
2025-03-10 14:37:29 +0700

    feat(db/schema): set up prisma adapter with sqlite

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,20 +1,16 @@
 import { z } from "zod";
 
-export const userSchema = z.object({
-  name: z.string().min(2).max(50),
+export const signInSchema = z.object({
   email: z.string().email({ message: "Invalid email" }),
   password: z
     .string()
     .min(8, { message: "Password must be at least 8 characters" }),
 });
 
-export const signInSchema = z.object({
-  email: z.string().email({ message: "Invalid email" }),
-  password: z.string().min(8, { message: "Password must be at least 8 characters" }),
-});
-
 export const signUpSchema = z.object({
   name: z.string().min(2).max(50),
   email: z.string().email({ message: "Invalid email" }),
-  password: z.string().min(8, { message: "Password must be at least 8 characters" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters" }),
 });
